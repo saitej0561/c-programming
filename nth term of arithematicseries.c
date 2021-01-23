a9db4238e4ca69f5a531c068c9b2124462dda59e
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-01-23 06:22:18 +0530

    Create nth term of arithematicseries.c

---
+++ nth term of arithematicseries.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a,n,d,nthterm;  // a is first term  ,,  n is nth terms ,,  d is common difference  //
+printf("enter the a , n , d values\n");
+scanf("%d%d%d",&a&n&d);
+nthterm = a+(n-1)*d;
+printf("nth term of airthematic series is %d", nthterm");
+}
