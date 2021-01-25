34b2afa9bda36c238058d6ae99564543cb206bd2
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-01-25 06:05:17 +0530

    Create typecating.c

---
+++ typecating.c
@@ -0,0 +1,12 @@
+using the casting operator
+
+#include<stdio.h>
+void main()
+{
+  int i=10;
+  float f;
+  f=(float)i;
+  printf("before type casting the value of i is %d",i);
+  printf("after type casting the value of i is %f",f);
+  }
+  
