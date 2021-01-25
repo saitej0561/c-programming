1638487fb19575a41baef805a6e5819d28cd5f9f
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-01-25 05:55:59 +0530

    Update nth term of arithematicseries.c

--- nth term of arithematicseries.c
+++ nth term of arithematicseries.c
@@ -5,7 +5,7 @@ void main()
 {
 int a,n,d,nthterm;  // a is first term  ,,  n is nth terms ,,  d is common difference  //
 printf("enter the a , n , d values\n");
-scanf("%d%d%d",&a&n&d);
+scanf("%d%d%d",&a,&n,&d);
 nthterm = a+(n-1)*d;
 printf("nth term of airthematic series is %d", nthterm");
 }
