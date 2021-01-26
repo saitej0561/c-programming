0b5495872068fe8035e1ed3c16fb53be0dcf79c5
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-01-27 05:23:15 +0530

    Create pre & post.c

---
+++ gatecs/types operations expressions/pre & post.c
@@ -0,0 +1,16 @@
+pre increment and post increment.c
+
+#include<stdio.h>
+void main()
+{
+  int x,a;
+  printf("enter a value\n");
+  scanf("%d",&n);
+  x=n++; /* post increment*/
+  printf("value of x is %d and n is %d",x,n);
+  
+  x=++n;  /* pre increment*/
+  printf("value of x is %d and n is %d",x,n);
+ }
+  
+  
