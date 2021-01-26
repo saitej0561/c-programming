ea1d33394c53b4c08a0135b92e07c5ca68f9e1da
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-01-26 05:48:24 +0530

    Create datatype sizes.c

---
+++ gatecs/types operations expressions/datatype sizes.c
@@ -0,0 +1,10 @@
+program which explains about the size of data type
+
+#include<stdio.h>
+void main()
+{
+  printf("size of int is %d",sizeof(int));
+  printf("size of char is %d byte",sizeof(char));
+  printf("size of float is %d byte",sizeof(float));
+  printf("size of double is %d byte",sizeof(double));
+}
