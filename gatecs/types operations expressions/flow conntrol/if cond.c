0ef33304b7a934429bafe9fd6d33cba518623f8e
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-01-27 05:04:30 +0530

    Create if cond.c

---
+++ gatecs/types operations expressions/flow conntrol/if cond.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  printf("enter a,b values");
+  scanf("%d %d",&a,&b);
+  if(a>b)
+  {
+    printf("a is grater");
+    }
+    else
+    {
+      printf("b is greater");
+      }
+ }
