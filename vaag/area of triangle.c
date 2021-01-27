f5f3150b5343b3060fcf219ee9ae24e830e9e37e
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-01-27 06:23:59 +0530

    Create area of triangle.c

---
+++ vaag/area of triangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int base,height,area;
+  printf("enter base and height value\n");
+  scanf("%d %d",&base,&height);
+  area=1/2*base*height;
+  printf("area of triangle is %d",area);
+  return 0;
+ }
