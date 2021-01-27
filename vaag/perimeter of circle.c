ee45d770b9af07e7523bea5e97d2fb388e75fee9
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-01-27 06:27:00 +0530

    Create perimeter of circle.c

---
+++ vaag/perimeter of circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define pi 3.14
+void main()
+{
+  int radius,perimeter;
+  printf("enter the value of radius");
+  scanf("%d",&radius);
+  perimeter=2*pi*radius;
+  printf("perimeter of circle is %d",perimeter);
+  
+ }
+  
