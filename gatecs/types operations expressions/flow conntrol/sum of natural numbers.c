11f199cb2cca33d21187942a1291ce0497290daa
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-02-03 05:27:40 +0530

    Create sum of natural numbers.c

---
+++ gatecs/types operations expressions/flow conntrol/sum of natural numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+  int num,sum=0,i;
+  printf("enter ur num\n");
+  scanf("%d",&num);
+  for(i=1;i<=num;i++)
+    {
+      sum=sum+i;
+      }
+    printf("sum is %d",sum);
+    }
