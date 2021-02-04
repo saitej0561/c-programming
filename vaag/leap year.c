688020ca0665a1f5019cb06b45a195eecc8c29aa
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-02-04 05:37:40 +0530

    Create leap year.c

---
+++ vaag/leap year.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int year;
+  printf("enter year\n");
+  scanf("%d",&year);
+  if(year%2==0)
+    {
+      printf("leap year");
+      }
+      else
+      {
+        printf("not a leap year");
+        }
+ }
