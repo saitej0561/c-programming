22d65fa912bb3acd2242adb59ea0e937166d5083
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-02-04 05:46:15 +0530

    Create break.c

---
+++ gatecs/types operations expressions/flow conntrol/break.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int i,num;
+  for(i=0;i<15;i++)
+    {
+      printf("enter the num");
+      scanf("%d",&num);
+      if(num<0)
+      {
+        break;
+        }
+        
+     }
+ }
