b035ef4043d5e23b88c6267b27c37a3ecd84a2b0
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-02-09 09:12:07 +0530

    Create count of number.c

---
+++ gatecs/types operations expressions/flow conntrol/count of number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+  int num,count=0,
+  printf("enter the num");
+  while(num!=0)
+      {
+          num=num/10;
+          ++count;
+      }
+    printf("count value is %d",count);
+    }
