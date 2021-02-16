13e8fb34171ad70e4a2e374af2259c7251015a3a
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-02-16 06:33:54 +0530

    Delete area.c

--- vaag/area.c
+++
@@ -1,9 +0,0 @@
-  #include<stdio.h>
-  void main()
-  {
-    int area,s,a,b,c;
-    printf("enter s,a,b,c values\n");
-    scanf("%d %d %d %d",&s,&a,&b,&c);
-    area=(sqrt(s*(s-a)*(s-b)*(s-c)));
-    printf("area is %d",area);
-    }
