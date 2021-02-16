9adcc0ce2e3e7615d4d4d71800a8cc1d26d461a7
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-02-16 06:32:55 +0530

    Delete gatecs/types operations expressions directory

--- gatecs/types operations expressions/assignment operator.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-  int i=1;
-  i+=2  /*  i = i+ 2;*/
-  printf("value of = %d ", i);
-  
-}
--- gatecs/types operations expressions/flow conntrol/do while.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-  {
-    int a=20;
-    do{
-      printf("a is %d",a);
-      a++;
-      }while(a<20);
-      }
