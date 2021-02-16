ea8e7b74327eb4199af6094a5dde92d0fbe9adc6
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-02-16 06:41:30 +0530

    Create simple intrest.c

---
+++ vaag/simple intrest.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+  int si,p,t,r;
+  printf("enter the p t r values\n");
+  scanf("%d %d %d",&p,&t,&r);
+  si=(p*t*r)/100;
+  printf("simple intrest =%d",si);
+  
+  }
