bb086c8323f044e0c8728c47463a6db6e57d4eb2
saitej0561 <77865229+saitej0561@users.noreply.github.com>
2021-02-16 06:35:13 +0530

    Delete y=ax8-bz2-c3.c

--- vaag/y=ax8-bz2-c3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-  int y,a,x,b,z,c;
-  printf("enter a,x,b,z,c values");
-  scanf("%d %d %d %d %d",&a,&x,&b,&z,&c);
-  y=a*pow(x,8)-b*pow(z,2)-pow(c,3);
-  printf("result y is %d",y);
-  }
